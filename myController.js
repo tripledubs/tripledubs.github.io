90d9c46d7860315e6c43a95f97ffa6d85c8f44e3
tripledubs <dwayne.page@gmail.com>
2016-04-22 00:17:25 -0500

    initial

---
+++ myController.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('myController', function ($scope,$http,myFactory) {
+
+            $scope.working = true;
+            $scope.users = [];
+
+            $scope.getData = function (url) {
+                return $http.get(url).then(
+                    function success(response) {
+                        $scope.worked = true
+                    },
+                    function failure(reason) {
+                        $scope.worked = false
+                    }
+                );
+            }
+
+            $scope.getFromFactory = function (url) {
+                myFactory.getData(url).then(
+                    function success() {
+                        $scope.worked = true
+                    }), function failure() {
+                        $scope.worked = false
+                    }
+            }
+
+            $scope.getFromFactoryGOOD = function (url) {
+                myFactory.getDataGOOD(url).then(
+                    function success() {
+                        $scope.worked = true
+                    }, function failure() {
+                        console.log("here");
+                        $scope.worked = false
+                    })
+            }
+
+            function initialize() {
+                var _url = "/common/timeZoneData.json";
+                return $http({
+                    method: 'GET',
+                    url: _url
+                }).then(function successCallback(response) {
+                    console.log("data loaded");
+                }).catch(function () {
+                    console.log("Data not loaded");
+                })
+            }
+
+        })
+
+})();
---
+++ myFactory.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('myFactory', function($http) {
+            var service = {
+                // Bad
+                getData: function (url) {
+                    return $http.get(url).then(
+                        function success(response) {
+                            console.log(response.data)
+                            return response.data
+                        },
+                        function failure(reason) {
+                            return reason
+                        }
+                    )
+                },
+                // good
+                getDataGOOD: function (url) {
+                    return $http.get(url).then(
+                        function success(response) {
+                            console.log(response.data)
+                            return response.data
+                        }
+                    )
+                }
+
+            } // end SErvice
+
+            return service;
+
+        })
+})();
